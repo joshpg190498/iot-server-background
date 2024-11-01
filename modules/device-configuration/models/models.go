2b81b6b8c950485eb968ec7d92a196d69de48e6f
joshpg190498 <joseperez1910498@gmail.com>
2024-11-01 14:20:05 -0500

    comments

--- modules/device-configuration/models/models.go
+++ modules/device-configuration/models/models.go
@@ -13,12 +13,12 @@ type Config struct {
 }
 
 type KafkaMessage struct {
-	ID                  int    `json:"id"`
-	IDDevice            string `json:"id_device"`
-	HashUpdate          string `json:"hash_update"`
-	Type                string `json:"type"`
-	CreationDatetimeUTC string `json:"creation_datetime_utc"`
-	UpdateDatetimeUTC   string `json:"update_datetime_utc"`
+	ID                  int    `json:"id"`                    // Identificador de la actualización
+	IDDevice            string `json:"id_device"`             // Identificador del dispositivo
+	HashUpdate          string `json:"hash_update"`           // Identificador del mensaje
+	Type                string `json:"type"`                  // Tipo de actualización
+	CreationDatetimeUTC string `json:"creation_datetime_utc"` // Fecha de creación de la actualización
+	UpdateDatetimeUTC   string `json:"update_datetime_utc"`   // Fecha de actualización del dispositivo
 }
 
 type DeviceReadingSetting struct {
